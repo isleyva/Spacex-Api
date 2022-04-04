0f9082e92b0d37b9a78fab204a703de3834d6caf
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-04-04 09:52:03 +0200

    apifetch

---
+++ spacex-app/src/services/api.js
@@ -0,0 +1,22 @@
+const API_URL = 'https://api.spacexdata.com/v3/launches';
+
+export async function getLaunches() {
+ try {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
+    } catch (error) {
+      console.error(error);
+    }
+
+}
+
+export async function getlaunchByFlightNumber(flightNumber) {
+  try {
+    const response = await fetch(`${API_URL}?flight_number=${flightNumber}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
