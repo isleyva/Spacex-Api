1987cedc1ff2969f0f004ee405297d21b25471d0
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-04-15 18:00:30 +0200

    squaring header

--- spacex-app/src/App.js
+++ spacex-app/src/App.js
@@ -14,7 +14,7 @@ export function App() {
   return  (
   <>
     <img src={logo} width={350} alt="SpaceX" />
-    <Heading as="h1" size="lg">
+    <Heading as="h1" size="lg" m={4}>
       SpaceX Launches
     </Heading>
     <section>
